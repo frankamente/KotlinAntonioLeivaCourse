122e732d6ca5171f2f89b6cd82e1e664ad82af6a
frankamente <fran29400@gmail.com>
2020-04-10 12:07:58 +0200

    Created first Adapter

--- app/src/main/java/com/example/myplayer/MainActivity.kt
+++ app/src/main/java/com/example/myplayer/MainActivity.kt
@@ -10,14 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.button)
-        val textInput = findViewById<TextView>(R.id.editText)
-        button.setOnClickListener{
-            showToast("Hello ${textInput.text}")
-        }
-    }
-
-    private fun showToast(text: String){
-        android.widget.Toast.makeText(applicationContext, text, android.widget.Toast.LENGTH_SHORT).show()
     }
 }
---
+++ app/src/main/java/com/example/myplayer/MediaAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.myplayer
+
+import android.media.browse.MediaBrowser
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+
+class MediaItem
+
+class MediaAdapter(val items: List<MediaItem>) : RecyclerView.Adapter<MediaAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.view_media_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(items[position])
+    }
+
+    override fun getItemCount(): Int = items.size
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(item: MediaItem){
+
+        }
+    }
+}
